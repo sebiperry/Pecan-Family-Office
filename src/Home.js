94f444e7f5615315d7a7100f0358f2075400312c
pssanchez <pssanchez@ucsd.edu>
2023-09-18 16:41:16 -0700

    fixed logo

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,10 @@ import logo from './perrylogo.png';
 function Home() {
   return (
     <div classname='home-container'>
-      <div className="image-wrapper">
+      <div>
         <img src={homeimage} alt="Home" />
         <img src={logo} alt="Logo" className="logo-image" />
       </div>
-      
       <div className="box-container">
         <a>Founded in 2015, Pecan Family Office is a capital provider strategically looking to generate alpha across the risk/reward spectrum and leverage existing skill sets into board-level or advisory/consulting roles as appropriate. </a>
       </div>
