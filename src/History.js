5c8a88ba7ea66cdcce06416a189bf1d1061015aa
sebiperry <sebiperry19@gmail.com>
2023-09-18 17:13:27 -0700

    History

--- src/History.js
+++ src/History.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Slideshow from './Slideshow.js';
+import './History.css';
 
 function History() {
   return (
-    <div>
-      {/* Add history content here */}
+    <div classname = 'history-container'>
+        <div classname = 'slideshow-container'>
+        <img src={logo} alt="Logo" className="logo-image" />
+            <Slideshow />
+            
+        </div>
     </div>
   );
 }
